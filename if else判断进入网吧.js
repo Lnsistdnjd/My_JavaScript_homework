ed4daca1c196c3f87efe312cd756e23a6e267176
Lnsist <107336980+Lnsistdnjd@users.noreply.github.com>
2022-10-12 21:23:19 +0800

    Add files via upload

---
+++ if else判断进入网吧.js
@@ -0,0 +1,10 @@
+var age = prompt('请输入您的年龄');
+	  if(age>=18){
+	  var cango = true;
+	  alert('您可以进入网吧'+'进入权限:'+cango);
+	  }
+	  else{
+	  var cango = false;
+	  alert('您未满18岁,不能进入网吧\n'+'进入权限:'+cango);
+	  }
+	  
